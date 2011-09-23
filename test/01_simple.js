fbcce2a3753b5100f9d4f6765034dac940df046a
sugyan <sugi1982@gmail.com>
2011-09-24 01:40:50 +0900

    nest test

--- test/01_simple.js
+++ test/01_simple.js
@@ -1,16 +1,28 @@
 var TestTCP = require('./../index');
+var net = require('net');
 
 exports.test_tcp = function (test) {
     TestTCP.test_tcp({
         client: function (port, callback) {
-            callback(function () {
-                test.done();
+            var socket = new net.Socket();
+            socket.connect(port, function () {
+                var write;
+                socket.on('close', function () {
+                    callback(function () {
+                        test.done();
+                    });
+                });
+                socket.on('data', function (data) {
+                    test.equal(data.toString(), 'foo', 'echo');
+                    socket.end();
+                });
+                socket.write('foo');
             });
         },
-        server: (function () {
-            var net = require('net');
-            return net.createServer(function (socket) {
+        server: net.createServer(function (socket) {
+            socket.on('data', function (data) {
+                socket.write(data);
             });
-        }())
+        })
     });
 };
---
+++ test/02_nest.js
@@ -0,0 +1,39 @@
+var TestTCP = require('./../index');
+var net = require('net');
+
+exports.test_tcp = function (test) {
+    TestTCP.test_tcp({
+        client: function (port, callback1) {
+            TestTCP.test_tcp({
+                client: function (port, callback2) {
+                    var socket = new net.Socket();
+                    socket.connect(port, function () {
+                        var write;
+                        socket.on('close', function () {
+                            callback2(function () {
+                                callback1(function () {
+                                    test.done();
+                                });
+                            });
+                        });
+                        socket.on('data', function (data) {
+                            test.equal(data.toString(), 'foo', 'echo');
+                            socket.end();
+                        });
+                        socket.write('foo');
+                    });
+                },
+                server: net.createServer(function (socket) {
+                    socket.on('data', function (data) {
+                        socket.write(data);
+                    });
+                })
+            });
+        },
+        server: net.createServer(function (socket) {
+            socket.on('data', function (data) {
+                socket.write(data);
+            });
+        })
+    });
+};
