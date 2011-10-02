73dad5bec6fb556f8d705865b8b5f63b05a97efb
sugyan <sugi1982@gmail.com>
2011-10-02 16:52:27 +0900

    refactoring, add tests

--- test/04_oo.js
+++ test/04_oo.js
@@ -25,3 +25,27 @@ exports.oo = function (test) {
         });
     });
 };
+
+exports.stop_without_callback = function (test) {
+    var server; server = new TestTCP.TestTCP({
+        code: net.createServer(function (socket) {
+            socket.on('data', function (data) {
+                socket.write(data);
+            });
+        })
+    });
+    server.on('start', function () {
+        var socket = new net.Socket();
+        socket.connect(server.port, function () {
+            socket.on('close', function () {
+                server.stop();
+                test.done();
+            });
+            socket.on('data', function (data) {
+                test.equal(data.toString(), 'foo', 'echo');
+                socket.end();
+            });
+            socket.write('foo');
+        });
+    });
+};
--- test/05_server_code.js
+++ test/05_server_code.js
@@ -74,3 +74,39 @@ exports.server_code_oo = function (test) {
         });
     });
 };
+
+exports.oo_stop_without_callback = function (test) {
+    var server = new TestTCP.TestTCP({
+        code: function (port, done) {
+            var tcp = require('child_process').spawn(
+                'node', [
+                    '-e',
+                    'require("net").createServer(function(s){s.on("data",function(d){s.write(d);})}).listen(' + port + ',function(){console.log("ok");});1;'
+                ]
+            );
+            tcp.stdout.on('data', function (chunk) {
+                if (/ok/.test(chunk)) {
+                    done();
+                }
+            });
+            return function (done) {
+                tcp.on('exit', done);
+                tcp.kill();
+            };
+        }
+    });
+    server.on('start', function () {
+        var socket = new net.Socket();
+        socket.connect(server.port, function () {
+            socket.on('close', function () {
+                server.stop();
+                test.done();
+            });
+            socket.on('data', function (data) {
+                test.equal(data.toString(), 'foo', 'echo');
+                socket.end();
+            });
+            socket.write('foo');
+        });
+    });
+};
